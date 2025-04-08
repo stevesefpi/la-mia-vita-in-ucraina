0dc7e7c076758f0a4905ee9d1f4ab196ebf8402c
Steven Fabio <stevenfabio95@gmail.com>
2025-04-09 00:29:16 +0300

    Fix minamed module import

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import styles from "./navbar.module.css";
+import styles from "./Navbar.module.css";
 
 import { Facebook, Instagram, Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
