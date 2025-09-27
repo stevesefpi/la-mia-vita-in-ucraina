8338753f0b23786f287ea9d88e1c867b117bc801
Steven Fabio <stevenfabio95@gmail.com>
2025-09-27 22:58:34 +0300

    Update sitemap with English links

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,10 +3,16 @@ import { fetchPosts } from "@/lib/fetchPosts";
 export default async function sitemap() {
   const posts = await fetchPosts();
 
-  const postEntries = posts.map((post) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post.id}`,
-    lastModified: new Date(post.created_at).toISOString(),
-  }));
+  const postEntries = posts.flatMap((post) => [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post.id}`, // IT
+      lastModified: new Date(post.created_at).toISOString(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/en/blog/${post.id}`, // EN
+      lastModified: new Date(post.created_at).toISOString(),
+    },
+  ]);
 
   return [
     {
@@ -14,7 +20,15 @@ export default async function sitemap() {
       lastModified: new Date().toISOString(),
     },
     {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/en`,
+      lastModified: new Date().toISOString(),
+    },
+    {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+      lastModified: new Date("2025-01-01T10:00:12Z").toISOString(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/en/about`,
       lastModified: new Date("2025-01-01T10:00:12Z").toISOString(),
     },
     {
@@ -22,7 +36,15 @@ export default async function sitemap() {
       lastModified: new Date("2025-03-01T12:30:06Z").toISOString(),
     },
     {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/en/blog`,
+      lastModified: new Date("2025-03-01T12:30:06Z").toISOString(),
+    },
+    {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/contatti`,
+      lastModified: new Date("2025-02-20T09:15:43Z").toISOString(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/en/contatti`,
       lastModified: new Date("2025-02-20T09:15:43Z").toISOString(),
     },
     {
@@ -30,7 +52,15 @@ export default async function sitemap() {
       lastModified: new Date("2025-04-10T18:00:22Z").toISOString(),
     },
     {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/en/traduzione-intervista`,
+      lastModified: new Date("2025-04-10T18:00:22Z").toISOString(),
+    },
+    {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/guide/viaggio-ucraina`,
+      lastModified: new Date("2025-06-22T14:17:19Z").toISOString(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/en/guide/viaggio-ucraina`,
       lastModified: new Date("2025-06-22T14:17:19Z").toISOString(),
     },
     ...postEntries,
